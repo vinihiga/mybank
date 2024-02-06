325a12bfce8d2ef77342bee49e417891762070ea
Vinicius Higa <viniciushigadev@gmail.com>
2024-02-05 21:06:44 -0300

    Testing simple remove duplicates algorithm

--- main.go
+++ main.go
@@ -2,6 +2,30 @@ package main
 
 import "fmt"
 
+func removeDuplicates(x *[]int) {
+	dict := make(map[int]int)
+	var i = 0
+
+	for i < len(*x) {
+		_, ok := dict[(*x)[i]]
+
+		if ok {
+			slice := make([]int, len(*x)-1)
+			copy(slice, (*x)[:i])
+			copy(slice[i:], (*x)[i+1:])
+			*x = slice
+			i--
+		} else {
+			dict[(*x)[i]] = 1
+		}
+
+		i++
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	x := make([]int, 0, 4)
+	x = append(x, 1, 1, 2, 3)
+	removeDuplicates(&x)
+	fmt.Println(x)
 }
